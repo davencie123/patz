f56d7b2b29951f40143f1cc68f4d7eb04a0d0b90
davencie123 <153158306+davencie123@users.noreply.github.com>
2025-12-03 02:55:32 +0800

    Create .js

---
+++ .js
@@ -0,0 +1,10 @@
+// Your JS file
+
+function checkoutSuccess() {
+    alert("Successfully Check Out");
+    window.location.href = ".html";  // your home page
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Page loaded successfully.");
+});
